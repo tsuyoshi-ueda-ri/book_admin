1f948b0e2cbbbbc13f1369d90f219d681c961cdd
上田　剛史 <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-05-27 22:10:26 +0900

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,7 +14,12 @@ class Book < ApplicationRecord
       "lovely #{matched}"
     end
   end
-  after_destroy do
-    Rails.logger.info "Book is deleted: #{self.attributes}"
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please check!!"
+  end
+
+  def high_price?
+    price >= 5000
   end
 end
