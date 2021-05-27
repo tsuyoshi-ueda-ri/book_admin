6941c36ba4a849eb2e1eb51a838f6d40a0950758
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-05-27 17:23:04 +0900

    アソシエーション作成
    書籍と出版社
    1対多でアソシエーション。

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000)}
   scope :written_about, -> (theme) { where("name like ?", "%#{theme}%")}
 
+  belongs_to :publisher
 end
